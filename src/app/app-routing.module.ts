7340c57a71237cd203b2baca090ca274cf122259
lucasalvess96 <lucasalves.57@hotmail.com>
2022-07-25 08:59:32 -0300

    Routes basic, children,wildcard,notfound

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ChildAComponent } from './child-a/child-a.component';
+import { ChildBComponent } from './child-b/child-b.component';
 
-const routes: Routes = [];
+import { FirstComponent } from './first/first.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SecondComponent } from './second/second.component';
+import { YourGuardGuard } from './your-guard.guard';
+
+const routes: Routes = [
+  { path: 'first-component', component: FirstComponent, children:[{ path: 'child-a', component: ChildAComponent },{ path: 'child-b',component: ChildBComponent }]},
+  { path: 'second-component', component: SecondComponent },
+  { path: '', redirectTo: '/first-component', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+  // { path: '/your-path', component: YourGuardGuard, canActivate: [YourGuardGuard] },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/your-guard.guard.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+//diz ao roteador para mediar a navegação para essa rota específica.
+export class YourGuardGuard implements CanActivate {
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return true;
+  }
+  
+}
